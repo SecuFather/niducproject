60ed83cceaf2e4d53924783862a92d52636ba835
Pawel Rejkowicz <pawel.rejkowicz@gmail.com>
2012-03-11 01:40:56 +0100

    1.0

---
+++ src/net/netii/niducproject/DBHelper.java
@@ -0,0 +1,85 @@
+package net.netii.niducproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+	public static final String DATABASE_NAME = "niduc_project_01.db";
+	public static final int DATABASE_VERSION = 1;
+	
+	public static final String NIDUC_TABLE = "niduc_project";
+	public static final String CASHDESK_ID = "cashdesk_id";
+	public static final String CARD_PAID = "card_paid";
+	public static final String SIZE = "shopping_size";
+	public static final String DATE = "shopping_date";
+	public static final String TIME = "shopping_time";		
+	
+	
+	public DBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);		
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase base) {
+		try {
+			Log.i("sql", "not-created");
+			base.execSQL("CREATE TABLE " + NIDUC_TABLE + "("
+						+ CASHDESK_ID + " int(2), "
+						+ CARD_PAID + " int(1), "
+						+ SIZE + " int(1), "
+						+ DATE + " datetime, "
+						+ TIME + " int(2), "
+					    + " PRIMARY KEY (" 
+					    + CASHDESK_ID + "," +  CARD_PAID + ","
+					    + SIZE + "," + DATE + "," + TIME + "));");
+			Log.i("sql", "created");
+		} catch (SQLiteException ex) {
+			Log.i("sql", ex.getMessage());
+		}
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase base, int arg1, int arg2) {
+
+	}	
+	
+	
+	public String insertData(String cashdeskId, String cardPaid, String size, String date, String time){
+		String query = "INSERT INTO " + NIDUC_TABLE
+					+ " VALUES(" + cashdeskId + "," + cardPaid + "," + size
+				          + ",'" + date + "'," + time + ");";
+				
+		try{
+			SQLiteDatabase base = getWritableDatabase();			
+			base.execSQL(query);
+		}catch(SQLiteException ex){
+			Log.i("sql", ex.getMessage());
+			return "Database error";
+		}
+        try{
+        	HttpClient httpclient = new DefaultHttpClient();
+        	
+            HttpPost httppost = new HttpPost("http://niducproject.netii.net/index.php");
+            
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("query", query));            
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));                                          
+        }catch(Exception e){
+        	Log.i("sql", e.getMessage());
+        }
+		return query;
+	}
+}
---
+++ src/net/netii/niducproject/NActivity.java
@@ -0,0 +1,157 @@
+package net.netii.niducproject;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class NActivity extends Activity implements OnClickListener{
+	final private int startB = 0;
+	final private int cardB = 1;
+	final private int sizeB = 2;
+	final private int sizeCount = 3;
+	final private int finishB = 5;
+	final private int buttonsByRow = 6;	
+	final private int n = 8;
+	private long time[] = new long[n];
+	
+    private LinearLayout layout;
+    private LinearLayout[] row = new LinearLayout[n+1];
+    private ToggleButton[] startShopping = new ToggleButton[n];
+    private TextView[] cashdeskId = new TextView[n];
+    private CheckBox[] cardPaid = new CheckBox[n];
+    private RadioGroup[] shoppingSize = new RadioGroup[n];
+    private Button[] finishShopping = new Button[n];    
+    private EditText segment;
+    private Button submit;
+                   
+    private DBHelper dbHelp;
+
+	public void initWidgets(){
+		layout = (LinearLayout)findViewById(R.id.main_layout);        	                      
+        
+        for(int i=0; i<n; ++i){        	
+        	
+        	row[i] = new LinearLayout(this);
+        	row[i].setOrientation(LinearLayout.HORIZONTAL);
+        	
+        	cashdeskId[i] = new TextView(this);
+        	cashdeskId[i].setText(String.valueOf(i));
+        	cashdeskId[i].setTextSize(30);
+        	row[i].addView(cashdeskId[i]);
+        	
+        	startShopping[i] = new ToggleButton(this);
+        	startShopping[i].setId(buttonsByRow*i + startB);
+        	startShopping[i].setOnClickListener(this);
+        	row[i].addView(startShopping[i]);        	
+        	
+        	cardPaid[i] = new CheckBox(this);
+        	cardPaid[i].setId(buttonsByRow*i+cardB);
+        	cardPaid[i].setOnClickListener(this);
+        	row[i].addView(cardPaid[i]);
+        	        	
+        	shoppingSize[i] = new RadioGroup(this);
+        	shoppingSize[i].setOrientation(RadioGroup.HORIZONTAL);
+        	for(int j=0; j<sizeCount; ++j){
+        		RadioButton tmpRadio = new RadioButton(this);
+        		tmpRadio.setId(buttonsByRow*i+j + sizeB);
+        		shoppingSize[i].addView(tmpRadio);        		
+        	}        	
+        	shoppingSize[i].check(buttonsByRow*i + sizeB);
+        	shoppingSize[i].setOnClickListener(this);
+        	row[i].addView(shoppingSize[i]);
+        	
+        	finishShopping[i] = new Button(this);
+        	finishShopping[i].setText("finish");
+        	finishShopping[i].setId(buttonsByRow*i + finishB);
+        	finishShopping[i].setOnClickListener(this);
+        	row[i].addView(finishShopping[i]);
+        	
+        	layout.addView(row[i]);        	        	
+        }
+        row[n] = new LinearLayout(this);
+    	row[n].setOrientation(LinearLayout.HORIZONTAL);
+    	
+    	segment = new EditText(this);
+    	segment.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+    	segment.setWidth(80);    	
+    	row[n].addView(segment);
+    	
+    	submit = new Button(this);
+    	submit.setText("set");
+    	submit.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				try{ 
+					int seg = Integer.parseInt(segment.getText().toString());
+					NActivity.this.setCashdeskId(seg);
+				}catch(NumberFormatException e){
+					
+				}				
+			}
+		});
+    	row[n].addView(submit);
+    	layout.addView(row[n]);
+	}
+
+    public void setCashdeskId(int seg) {		
+		for(int i=0; i<n; ++i){
+			cashdeskId[i].setText(String.valueOf(seg+i));
+		}
+	}
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);                
+        setContentView(R.layout.main);                
+        initWidgets();        
+        
+        dbHelp = new DBHelper(this);                       
+    }
+
+    public String generateStr(int at){
+    	int size = shoppingSize[at].getCheckedRadioButtonId() % buttonsByRow - sizeB;
+    	
+    	return dbHelp.insertData(cashdeskId[at].getText().toString(),
+    							 cardPaid[at].isChecked() ? "1" : "0",
+    							 String.valueOf(size),
+    							 Calendar.getInstance().getTime().toString(),
+    							 String.valueOf(time[at]));    					
+    }
+
+	@Override
+	public void onClick(View v) {
+		int x = v.getId() % buttonsByRow;
+		int y = v.getId() / buttonsByRow;
+		
+		switch(x){
+		case startB:
+			if(startShopping[y].isChecked()){
+				time[y] = Calendar.getInstance().getTime().getTime();
+			}
+			break;
+		case cardB:
+			
+			break;
+		case finishB:
+			if(startShopping[y].isChecked()){
+				time[y] = (Calendar.getInstance().getTime().getTime() - time[y])/1000;
+				Toast.makeText(this, generateStr(y), Toast.LENGTH_LONG).show();
+				startShopping[y].setChecked(false);
+			}
+			break;		
+		}		
+	}
+}
